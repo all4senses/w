e1cf35f83f5aeb38f6122d4d2baf5fbdd6adc435
Alexander Khrapovitskiy <all4senses@gmail.com>
2016-10-25 22:59:16 +0700

    Product list page

--- themes/amo_theme/src/js/layout/products_and_lists.js
+++ themes/amo_theme/src/js/layout/products_and_lists.js
@@ -1,5 +1,6 @@
 (function($){'use strict';Drupal.behaviors.products_and_list={attach:function(context,settings){
 
+    console.log('xxx...');
     $('body').on('click', '.product-filter-boutique a', function(event) {
         event.preventDefault();
         console.log('click filter...');
