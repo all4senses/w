95568e553db182d30d0f1258c95b7dc743e9d503
Michael Dajewski <michael.dajewski@j-apps.pl>
2016-11-06 13:43:19 -0200

    REVERT: Boutique page filter block mobile - fix

--- themes/amo_theme/src/js/layout/products_and_lists.js
+++ themes/amo_theme/src/js/layout/products_and_lists.js
@@ -1,58 +1,57 @@
-(function ($) {
-  'use strict';
-  Drupal.behaviors.products_and_list = {
-    attach: function (context, settings) {
+(function($){'use strict';Drupal.behaviors.products_and_list={attach:function(context,settings){
 
-      $('body').on('click', '#product-filter-boutique-2 a.toggle-filter', function (event) {
+
+    $('body').on('click', '#product-filter-boutique a.toggle-filter', function(event) {
         event.preventDefault();
 
         var e = $(this);
         e.toggleClass('opened');
-        e.parent().find('.filter-select').toggleClass('hidden').toggle();
-      });
-
-      $('body').on('click', "[id^='product-filter-boutique']" + ' a.toggle-products', function (event) {
+        e.parent().find('ul').toggleClass('hidden').toggle();
+    });
+    
+    
+    $('body').on('click', '#product-filter-boutique a.toggle-products', function(event) {
         event.preventDefault();
 
         var e = $(this);
         e.toggleClass('active');
-        update_products_view(e);
-      });
-
-      function update_products_view(e) {
-        var filter_block = '#' + e.closest("[id^='product-filter-boutique']").attr('id');
+        update_products_view();
+    });
+    
+    function update_products_view() {
         // Filter logic: OR
         var color, chateau;
-        var links = $(filter_block + ' a');
-        var links_active = $(filter_block + ' a.active');
-        var links_inactive = $(filter_block + ' a:not(.active)');
-
+        var links = $('#product-filter-boutique a');
+        var links_active = $('#product-filter-boutique a.active');
+        var links_inactive = $('#product-filter-boutique a:not(.active)');
+        
         if (links.length == links_active.length || links.length == links_inactive.length) {
-          // Show all products
-          $('.page-boutique .product.teaser.hidden').removeClass('hidden');
+            // Show all products
+            $('.page-boutique .product.teaser.hidden').removeClass('hidden');
         }
         else {
-          // Hide/show some products
-          // First, hide inactive
-          links_inactive.each(function (index) {
-            if (color = $(this).data('color')) {
-              $('.page-boutique .product.teaser:not(.hidden)[data-color="' + color + '"]').addClass('hidden');
-            }
-            else if (chateau = $(this).data('chateau')) {
-              $('.page-boutique .product.teaser:not(.hidden)[data-chateau="' + chateau + '"]').addClass('hidden');
-            }
-          });
-          // Second, show active
-          links_active.each(function (index) {
-            if (color = $(this).data('color')) {
-              $('.page-boutique .product.teaser.hidden[data-color="' + color + '"]').removeClass('hidden');
-            }
-            else if (chateau = $(this).data('chateau')) {
-              $('.page-boutique .product.teaser.hidden[data-chateau="' + chateau + '"]').removeClass('hidden');
-            }
-          });
+            // Hide/show some products
+            // First, hide inactive
+            links_inactive.each(function(index) {
+                if (color = $(this).data('color')) {
+                    $('.page-boutique .product.teaser:not(.hidden)[data-color="' + color + '"]').addClass('hidden');
+                }
+                else if (chateau = $(this).data('chateau')) {
+                    $('.page-boutique .product.teaser:not(.hidden)[data-chateau="' + chateau + '"]').addClass('hidden');
+                }
+            });  
+            // Second, show active
+            links_active.each(function(index) {
+                if (color = $(this).data('color')) {
+                     $('.page-boutique .product.teaser.hidden[data-color="' + color + '"]').removeClass('hidden');
+                }
+                else if (chateau = $(this).data('chateau')) {
+                    $('.page-boutique .product.teaser.hidden[data-chateau="' + chateau + '"]').removeClass('hidden');
+                }
+            });  
         }
-      }
+        
+        
     }
-  }
-})(jQuery);
+
+}}})(jQuery);
